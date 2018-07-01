3a503af53aa32822e2b5cb2db28415d9e1a162cf
Moomba Muloba <moomba.muloba@cohaesus.co.uk>
2018-07-01 19:45:53 +0100

    Stop book results from showing when the search query is empty

--- src/components/BookResults.js
+++ src/components/BookResults.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Book from './Book.js'
 import * as BooksAPI from '../BooksAPI'
 
 class BookResults extends React.Component {
@@ -9,36 +10,48 @@ class BookResults extends React.Component {
 
   componentDidUpdate(prevProps) {
     if (this.props.search !== prevProps.search) {
-      BooksAPI.search(this.props.search).then(books => {
-        this.setState({ searchResults: books });
-      }).catch(function (err) {
-        console.log("there was a search error", err);
-      });
+      // run code if the search props has a value
+      if (this.props.search) {
+        // ajax request based on search props
+        BooksAPI.search(this.props.search).then(books => {
+          // update search results if we get a valid response
+          if (books.length > 0 && this.props.search) {
+            this.setState({ searchResults: books });
+          } else {
+            this.setState({ searchResults: [] });
+          }
+        }).catch(function (err) {
+          console.log("there was a search error", err);
+        });
+      } else {
+        console.log("search result is empty")
+        this.setState({ searchResults: [] });
+      }
     }
   }
 
   render() {
     console.log("[BookResults]", this.state.searchResults);
+    const results = this.state.searchResults;
     return (
-      <h2>Hi there. I will be the book results</h2>
+      <ol className="books-grid">
+        {
+          results.map((book, index) => {
+            return <Book url={book.imageLinks.smallThumbnail}
+              title={book.title}
+              authors={book.authors}
+              bookId={book.id}
+              handleBookUpdate={this.handleBookUpdate}
+              key={index} />
+          })
+        }
+      </ol>
     )
   }
 }
 
 export default BookResults;
 
-{/* <ol className="books-grid">
-  {
-    results.map((book, index) => {
-      return <Book url={book.imageLinks.smallThumbnail}
-        title={book.title}
-        authors={book.authors}
-        bookId={book.id}
-        handleBookUpdate={this.handleBookUpdate}
-        key={index} />
-    })
-  }
-</ol> */}
 
-// TODO: manage the search results
-// Should we be making an AJAX request on every entry or only if the search query matches one of the results
+
+// TODO: tidy up searchResults code
